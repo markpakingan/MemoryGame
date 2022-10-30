9433c5fa62922160ffea948040dce0b3704b41b4
markpakingan <marklee024@gmail.com>
2022-10-29 19:42:35 -0700

    change bg color to black

--- VS Code Testing/kikiam.js
+++ VS Code Testing/kikiam.js
@@ -1,27 +1,13 @@
 const h1 = document.querySelector("h1");
 h1.style.color = "red";
 
-<<<<<<< HEAD
-// const paragraph = document.querySelector("p");
-
-// paragraph.style.color = "yellow";
-
-function randomRGB() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-=======
 function randomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = 0;
->>>>>>> try-purple
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r},${g},${b})`
 }
 
-<<<<<<< HEAD
-const letters = document.querySelectorAll(".letter");
-
-=======
 // setInterval(function () {
 //     h1.style.color = randomRGB();
 // }, 500);
@@ -29,13 +15,8 @@ const letters = document.querySelectorAll(".letter");
 const letters = document.querySelectorAll(".letter");
 
 
->>>>>>> try-purple
 const intervalId = setInterval(function () {
     for (let char of letters) {
         char.style.color = randomRGB();
     }
-<<<<<<< HEAD
 }, 500);
-=======
-}, 500);
->>>>>>> try-purple
